4519b0801531648f8887612eb1ea864341055f14
techis-staff <staff@techis.jp>
2021-03-29 20:10:09 +0900

    完成

--- wordpress-heroku-master/bin/cron/wordpress.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Running WordPress Cronjob using the Heroku Scheduler ..."
-export WP_CACHE=false
-cd $HOME
-
-# Run cronjob using wp-cli
-vendor/bin/wp cron event run --all
--- wordpress-heroku-master/config/environments/development.php
+++
@@ -1,8 +0,0 @@
-<?php
-/** Development */
-define('SAVEQUERIES', true);
-define('WP_DEBUG', true);
-define('SCRIPT_DEBUG', true);
-
-/** Plugins */
-define('S3_UPLOADS_USE_LOCAL', true);
